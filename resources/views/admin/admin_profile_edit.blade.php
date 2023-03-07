e76dd56803d276621634a1c999046fa65615faa9
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2023-03-06 20:45:57 -0800

    adding some javascript to update an image to a profile

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <div class="page-content">
     <div class="container-fluid">
          <div class="row">
@@ -34,10 +35,18 @@
                     <div class="row mb-3">
                         <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
                         <div class="col-sm-10">
-                            <input class="form-control" name="profile_image" type="file" value="{{$editData->email}}" id="name">
+                            <input class="form-control" name="profile_image" type="file"  id="image">
                         </div>
                     </div>
                     <!-- end row -->
+                    <div class="row mb-3">
+                        <label for="example-text-input" class="col-sm-2 col-form-label"></label>
+                        <div class="col-sm-10">
+                        <img id="showImage" class="rounded avatar-lg" src="{{asset('assets/images/small/img-1.jpg')}}" alt="Card image cap">
+                        </div>
+                    </div>
+                    <!-- end row -->
+                    <input type="submit" class="btn btn-info waves-effect waves-light" value="Update Profile">
                     </form>
                 </div>
             </div>
@@ -47,5 +56,20 @@
     </div>
 
 </div>
+
+<script type="text/javascript">
+
+    $(document).ready(function(){
+        $('#image').change(function(event){
+            var reader = new FileReader();
+            reader.onload = function(event){
+                $('#showImage').attr('src', event.target.result);
+            }
+            reader.readAsDataURL(event.target.files['0']);
+        })
+    })
+
+
+</script>
 
 @endsection

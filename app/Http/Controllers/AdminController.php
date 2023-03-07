3462971e642105bf46409250dec1145eac8e0901
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2023-03-07 14:37:56 -0800

    created a view to update user password

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,8 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class AdminController extends Controller
 {
 
@@ -37,6 +39,7 @@ class AdminController extends Controller
         return view('admin.admin_profile_edit', compact('editData'));
     }
 
+    // Edit Profile Method that allows you to update a profile image
     public function storeProfile(Request $request): RedirectResponse {
         $id = Auth::user()->id;
         $data = User::find($id);
@@ -61,5 +64,33 @@ class AdminController extends Controller
         ];
 
         return redirect()->route('admin.profile')->with($notification);
+    }
+
+    // Method View to update User Password
+    public function changePassword (){
+
+        return view('admin.admin_change_password');
+    }
+
+    // Post Method to update user password
+    public function updatePassword(Request $request) {
+        $validateData = $request->validate([
+            'oldpassword' => 'required',
+            'newpassword' => 'required',
+            'confirm_password' => 'required|same:newpassword',
+        ]);
+
+        $password = Auth::user()->password;
+        if(Hash::check($request->oldpassword, $password)){
+            $users = User::find(Auth::id());
+            $users->password = Hash::make($request->newpassword);
+            $users->save();
+
+            session()->flash('message', 'Password Updated Successfully');
+            return redirect()->back();
+        }else{
+            session()->flash('message', ' Old Password Does Not Match');
+            return redirect()->back();
+        }
     }
 }

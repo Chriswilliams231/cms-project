cbc711d8f584bb8db3d1c7b488061ef4b9f56704
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2023-03-16 17:46:05 -0700

    update

--- resources/views/admin/about_page/all_multiimage.blade.php
+++ resources/views/admin/about_page/all_multiimage.blade.php
@@ -7,8 +7,9 @@
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+             
                     <h4 class="mb-sm-0">All Multi Images</h4>
-
+                 
                 </div>
             </div>
         </div>
@@ -18,9 +19,9 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-
-                        <h4 class="card-title">All Multi Images</h4>
-                        
+                        <center>
+                            <h4 class="card-title">All Multi Images</h4><br><br>
+                         </center>
 
                         <table id="datatable" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                             <thead>

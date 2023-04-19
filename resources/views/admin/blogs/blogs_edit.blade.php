02bfacd0f1703bc32124e419b7ff072cd00a6b64
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2023-04-19 14:07:40 -0700

    small change

--- resources/views/admin/blogs/blogs_edit.blade.php
+++ resources/views/admin/blogs/blogs_edit.blade.php
@@ -1,5 +1,10 @@
 @extends('admin.admin_master')
 @section('admin')
+
+@section('dashboard')
+ Dashboard | Edit Blog 
+@endsection
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 <style type="text/css">

bf02c39404d0e71a8c30610aba275cec99953466
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2023-04-19 11:55:29 -0700

    making a few more updates

--- resources/views/frontend/blog.blade.php
+++ resources/views/frontend/blog.blade.php
@@ -2,7 +2,7 @@
 @section('main')
 
 @section('title')
- Blog Page
+ All Blogs
 @endsection
 
 <main>
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,7 +2,7 @@
 @section('main')
 
 @section('title')
- Home Portfolio Page
+ Home  Page
 @endsection
 
 <!-- banner-area -->
